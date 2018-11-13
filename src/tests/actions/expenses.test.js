6fbbd43336fb270e804c86e2d4232d5eab0f6cd1
Nithin Ashok <nithin.a333@gmail.com>
2018-11-13 00:36:46 -0800

    Setup test database env

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,4 +1,10 @@
-import { addExpense, editExpense, removeExpense } from "../../actions/expenses";
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { startAddExpense ,addExpense, editExpense, removeExpense } from "../../actions/expenses";
+import expenses from '../fixtures/expenses';
+import database from '../../firebase/firebase';
+
+const createMockStore = configureMockStore([thunk]);
 
 test('should setup remove expense action object', () => {
     const action = removeExpense({ id: '123abc'});
@@ -20,34 +26,77 @@ test('should setup edit expense object', () => {
 });
 
 test('should setup add expense object with provided vaules', () => {
+    const action = addExpense(expenses[2]);
+
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: expenses[2]
+    });
+});
+
+test('should add expense to database and store', (done) => {
+    const store = createMockStore({});
     const expenseData = {
-        description: 'Rent',
-        amount: 118000,
-        createdAt: 1000,
-        note: 'This is Oct month rent'
-    }
-    const action = addExpense(expenseData);
+        description: 'GoPro',
+        amount: 40000,
+        note: 'Hero7 has the best features!',
+        createdAt: 1000
+    };
 
-    expect(action).toEqual({
-        type: 'ADD_EXPENSE',
-        expense: {
-            ...expenseData,
-            id: expect.any(String)
-        }
+    store.dispatch(startAddExpense(expenseData)).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'ADD_EXPENSE',
+            expense: {
+                id: expect.any(String),
+                ...expenseData
+            }
+        });
+
+        return database.ref(`expenses/${actions[0].expense.id}`).once('value');
+    }).then((snapshot) => {
+        expect(snapshot.val()).toEqual(expenseData);
+        done();
     });
 });
 
-test('should setup add expense object with default vaules', () => {
-    const action = addExpense();
+test('should add expense with defaults to database and store', (done) => {
+    const store = createMockStore({});
+    const expenseDefaults = {
+        description: '',
+        note: '',
+        amount: 0,
+        createdAt: 0
+    };
 
-    expect(action).toEqual({
-        type: 'ADD_EXPENSE',
-        expense: {
-            id: expect.any(String),
-            description: '',
-            amount: 0,
-            createdAt: 0,
-            note: ''
-        }
+    store.dispatch(startAddExpense({})).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'ADD_EXPENSE',
+            expense: {
+                id: expect.any(String),
+                ...expenseDefaults
+            }
+        });
+
+        return database.ref(`expenses/${actions[0].expense.id}`).once('value');
+    }).then((snapshot) => {
+        expect(snapshot.val()).toEqual(expenseDefaults);
+        done();
     });
 });
+
+// test('should setup add expense object with default vaules', () => {
+//     const action = addExpense();
+
+//     expect(action).toEqual({
+//         type: 'ADD_EXPENSE',
+//         expense: {
+//             id: expect.any(String),
+//             description: '',
+//             amount: 0,
+//             createdAt: 0,
+//             note: ''
+//         }
+//     });
+// });
--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,7 +1,10 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import DotEnv from 'dotenv';
 
 //Configure enzyme: To use the correct adapter for the react version 
 Enzyme.configure({
     adapter: new Adapter()
 });
+
+DotEnv.config({ path: '.env.test' });
